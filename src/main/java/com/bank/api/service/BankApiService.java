f7dc634f7145b728309d465f67d5a85d4f29209b
eonyedikachi <ibeabuchionyedikachi@gmail.com>
2023-02-04 12:51:17 +0100

    Added Spring Boot Maven Structure

---
+++ src/main/java/com/bank/api/service/BankApiService.java
@@ -0,0 +1,13 @@
+package com.bank.api.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApiService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankApiService.class, args);
+    }
+
+}
---
+++ src/test/java/com/bank/api/service/BankApiServiceTests.java
@@ -0,0 +1,13 @@
+package com.bank.api.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiServiceTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
